0f16e2c57deb58ba97d3d4cb7cd3947e7357a2fa
Алекс <@DESKTOP-1ANSOAM>
2023-10-08 07:38:18 +0600

    Добавьте файлы проекта.

---
+++ SU.Online.Web/Program.cs
@@ -0,0 +1,27 @@
+﻿using SU.BLL;
+using SU.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SU.Online.Web
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Email = "alexnurbolat@gmail.com";
+            client.Password = "asd";
+            client.FName = "Zhursinbek";
+            client.SName = "Nurbolat";
+            
+
+            ServiceClient service = new ServiceClient();
+            service.RegisterClient(client);
+        }
+    }
+}
